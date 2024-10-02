c78f4eae939367cbea7bc1fe4f1c28c481bd83b3
Navid_Rahmani <navidrahmani770@gmail.com>
2024-10-03 00:37:24 +0330

    register back

--- Goldis/Goldis/urls.py
+++ Goldis/Goldis/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls')),
-    path('accounts/', include('accounts.urls')),
+    path('', include('home.urls', namespace='home')),
+    path('accounts/', include('accounts.urls', namespace='accounts')),
 
 ]
--- Goldis/accounts/forms.py
+++ Goldis/accounts/forms.py
@@ -1,7 +1,16 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 class UserRegistrationForm(forms.Form):
-    username = forms.CharField()
-    phone_number = forms.IntegerField()
-    password = forms.CharField()
-    confirm_password = forms.CharField()
+    username = forms.CharField(label='نام کابری',)
+    phone_number = forms.CharField(label='شماره تلفن',)
+    password = forms.CharField(label='رمز عبور', widget=forms.PasswordInput(attrs={'placeholder':'رمز عبور'}))
+    confirm_password = forms.CharField(label='تکرار رمز عبور', widget=forms.PasswordInput(attrs={'placeholder':'تکرار رمز عبور'}))
+
+    def clean(self):
+        cd = super().clean()
+        p1 = cd.get('password')
+        p2 = cd.get('confirm_password')
+        if p1 and p2 and p1 != p2:
+            raise ValidationError('رمز عبور همخوانی ندارد')
