8c6068c7b63e8c832aac7fbee9cef6c3616049fb
Faez <faezbehnoei99@gmail.com>
2024-10-10 00:23:04 +0330

    Error pages (urls , views)

--- Goldis/home/urls.py
+++ Goldis/home/urls.py
@@ -4,4 +4,9 @@ from . import views
 app_name = 'home'
 urlpatterns = [
     path('', views.home, name='home'),
+    path('404', views.notfound, name='Error404'),
+    path('400', views.badreq, name='Error400'),
+    path('403', views.permission, name='Error403'),
+    path('500', views.serverissue, name='Error500'),
 ]
+
--- Goldis/home/views.py
+++ Goldis/home/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home/index.html')
+
+def notfound(request):
+    return render(request, 'home/Error404.html')
+
+def badreq(request):
+    return render(request, 'home/Error400.html')
+
+def permission(request):
+    return render(request, 'home/Error403.html')
+
+def serverissue(request):
+    return render(request, 'home/Error500.html')
