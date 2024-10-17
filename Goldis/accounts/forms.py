e1ed3692bc24d823d44c5f514d0139f9f7593ea1
Mohammad Sepehr Bazargan <mohammadsepehr.bazargan@gmail.com>
2024-10-17 11:53:07 +0330

    login with phone number

--- Goldis/accounts/forms.py
+++ Goldis/accounts/forms.py
@@ -3,9 +3,9 @@ from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
 class UserLoginForm(forms.Form):
-    username = forms.CharField(min_length=5, label='', error_messages = {
-                 'required':"لطفا نام کاربری خود را وارد کنید"
-                 }, widget=forms.TextInput(attrs={'placeholder':'نام کاربری', 'class':''}))
+    phone_number = forms.CharField(min_length=11, max_length=11, error_messages = {
+                'required':"لطفا شماره تلفن خود را وارد کنید"},
+                label='', widget=forms.TextInput(attrs={'placeholder':'شماره تلفن', 'class':''}))
     password = forms.CharField(min_length=8, label='', error_messages = {
                  'required':"لطفا رمز عبور خود را وارد کنید"
                  }, widget=forms.PasswordInput(attrs={'placeholder':'رمز عبور', 'class':''}))
@@ -37,15 +37,3 @@ class UserRegistrationForm(forms.Form):
         p2 = cd.get('confirm_password')
         if p1 and p2 and p1 != p2:
             raise ValidationError('رمز عبور همخوانی ندارد')
-
-
-
-class UserLoginForm(forms.Form):
-    username = forms.CharField(min_length=5, label='', error_messages = {
-                 'required':"لطفا نام کاربری خود را وارد کنید",
-        'min_length': "نام کاربری باید حداقل ۵ حرف باشد"
-                 }, widget=forms.TextInput(attrs={'placeholder':'نام کاربری', 'autofocus': 'true', 'class':'inputs'}))
-    password = forms.CharField(min_length=8, label='', error_messages = {
-                 'required':"لطفا رمز عبور خود را وارد کنید",
-        'min_length': "رمز عبور باید حداقل ۸ حرف باشد",
-                 }, widget=forms.PasswordInput(attrs={'placeholder':'رمز عبور', 'class':'inputs'}))
