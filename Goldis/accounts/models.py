ef55413546fb815912b48f4a14644ba065f94788
Mohammad Sepehr Bazargan <mohammadsepehr.bazargan@gmail.com>
2024-10-17 11:53:38 +0330

    define user model

--- Goldis/accounts/models.py
+++ Goldis/accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Account(models.Model):
+    User_ID = models.BigAutoField(primary_key=True)  
+    phone_number = models.CharField(max_length=14)
+    address = models.TextField(null=True, blank=True)
+    password = models.CharField(max_length=128) 
+    user_type = models.IntegerField(null=True, blank=True)  
+    national_ID = models.CharField(max_length=10 , null=True, blank=True)
+    birthdate = models.DateField(null=True, blank=True)
+    rial_balance = models.CharField(max_length=10)
+    gold_balance = models.CharField(max_length=10)
