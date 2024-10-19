df27f47330ef82fd8d11cf4f97e94f9124eb4ef5
Navid_Rahmani <navidrahmani770@gmail.com>
2024-10-20 00:33:15 +0330

    forms validation

--- Goldis/utils.py
+++ Goldis/utils.py
@@ -1,18 +1,14 @@
-from http.client import responses
+from melipayamak import Api
 
-from kavenegar import *
 
 def send_otp_code(phone_number, code):
-    try:
-        api = KavenegarAPI('33323265434643785A4B6E316F4C6E48454357714272547A497A46784232587338685877326631395848553D')
-        params = {
-            'sender':'',
-            'receptor': phone_number,
-            'message': f'{code}کد تایید شما '
-        }
-        response = api.sms_send(params)
-        print(response)
-    except APIException as e:
-        print(e)
-    except HTTPException as e:
-        print(e)
+
+    username = '93344440483'
+    password = '84b7dc5'
+    api = Api(username, password)
+    sms = api.sms()
+    to = f'{phone_number}'
+    _from = '1000100120'
+    text = f'{code}رمز حساب کابری '
+    response = sms.send(to, _from, text)
+    print(response)
