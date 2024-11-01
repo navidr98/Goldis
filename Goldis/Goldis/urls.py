dc1c6e1ef6d443514fe43eb01df15600d580b781
Mohammad Sepehr Bazargan <mohammadsepehr.bazargan@gmail.com>
2024-11-01 15:35:33 +0330

    add sales app urls

--- Goldis/Goldis/urls.py
+++ Goldis/Goldis/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls', namespace='accounts')),
     path('', include('home.urls', namespace='home')),
+    path('sales/', include('sales.urls')),
 
 ]
